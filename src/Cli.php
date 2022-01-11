7801ad418f82e2a760c04179cf04c3a780aadb7a
Dmitry Zhdanov <signmark@gmail.com>
2022-01-11 19:11:07 +0300

    Fixed linter

--- src/Cli.php
+++ src/Cli.php
@@ -5,11 +5,11 @@ namespace Brain\Games\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function welcome(){
+function welcome()
+{
       line('Welcome to the Brain Game!');
       $name = prompt('May I have your name?');
       line("Hello, %s!", $name);
 
       return $name;
 }
-
--- src/Even.php
+++ src/Even.php
@@ -5,28 +5,30 @@ namespace Brain\Games\Even;
 use function cli\line;
 use function cli\prompt;
 
-function AskEven($name){
+function AskEven($name)
+{
     $good = 0;
-    for($i = 1; $i <= 3; $i++){
+    for ($i = 1; $i <= 3; $i++) {
         line('Answer "yes" if the number is even, otherwise answer "no".');
         $randNumber = rand(1, 100);
         $answer = prompt("Question: {$randNumber}?");
 
         $isEven = $randNumber % 2 === 0;
-        if($answer !== "no" && $answer !== "yes"){
+        if ($answer !== "no" && $answer !== "yes") {
             line("Your enter is incorrect!");
             $i--;
             continue;
         }
-        if(($answer === "yes" && $isEven) || ($answer === "no" && !$isEven)) {
+        if (($answer === "yes" && $isEven) || ($answer === "no" && !$isEven)) {
             line("Correct!");
             $good++;
-        }else{
+        } else {
                 line("'Yes' is wrong answer ;(. Correct answer was 'No'");
                 line("Let's try again, {$name}");
         }
     }
 
-    if($good === 3)
+    if ($good === 3) {
         line("Congratulations, %s!", $name);
+    }
 }
